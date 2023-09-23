60ead1be84d6cfbfb2e91b3a8d85680aeffccf3a
Kostya9911 <110036410+Kostya9911@users.noreply.github.com>
2023-09-23 15:08:05 +0200

    fixed style

--- src/components/Cast/Cast.styled.js
+++ src/components/Cast/Cast.styled.js
@@ -2,7 +2,12 @@ import styled from 'styled-components';
 
 export const List = styled.ul`
   list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: 5px;
+  /* gap: 5px; */
   > li {
     margin-bottom: 20px;
+    width: 180px;
   }
 `;
